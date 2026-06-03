08c6686699206c10105a3e9a1ceaa111360d02d8
Robin Hayman <robin.hayman@gmail.com>
2026-06-03 15:01:35 +0100

    added debugging messages

--- StimControl.cpp
+++ StimControl.cpp
@@ -203,17 +203,8 @@ void StimControl::sendData() {
 }
 
 int StimControl::sendStringToDevice(std::string const &str) {
-  int n = str.length();
-  unsigned char arr[n + 1];
-  auto first = str.begin();
-  auto last = str.end();
-
-  std::copy(first, last, arr);
-  arr[n] = '\0'; // Ensure null-termination
-
-  // std::vector<unsigned char> cstr(str.data(), str.data() + str.size() + 1);
-  // return serial.writeBytes(&cstr[0], sizeof(str));
-  return serial.writeBytes(arr, n);
+  std::vector<unsigned char> buffer(str.begin(), str.end());
+  return serial.writeBytes(buffer.data(), buffer.size());
 }
 
 void StimControl::deviceInitialized(bool val) { isDeviceSetup = val; }
